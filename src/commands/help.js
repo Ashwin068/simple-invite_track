c7a16f61c95db04586f2c691efbacda1219cba26
Ashwin <56057445+Ashwin1172@users.noreply.github.com>
2019-11-29 02:15:05 +0530

    Add files via upload

---
+++ src/commands/help.js
@@ -0,0 +1,18 @@
+import Discord from 'discord.js';
+
+const help = (bot, msg, cmd) => {
+  const richEmbed = new Discord.RichEmbed();
+
+  msg.channel.send({
+    embed: richEmbed
+            .setColor('#ffffff')
+            .setDescription(`${"`" + ">invites" + "`"} - **check # invites until next role**
+${"`" + ">top <#>" + "`"} - **check a certain # of top inviters**
+${"`" + ">membercount" + "`"} - **check total members in server**
+${"`" + ">set <#channel-name>" + "`"} - **set the bot channel**
+${"`" + ">set default" + "`"} - **allow bot in all channels**
+**Made by CAPTAIN COOL#0786.**`)
+  });
+}
+
+export default help;
